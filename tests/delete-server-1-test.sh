136d72574eafbf49c9d307a0768bac5752bf300c
connaryscott <connaryscott@gmail.com>
2013-01-07 10:33:59 -0800

    add new commands add-server and delete-server add polling support for synchronous behavior

---
+++ tests/delete-server-1-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the `delete-server` command.
+#    
+#/ usage:  rerun stubbs:test -m gogrid -p delete-server [--answers <>]
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "delete-server"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
---
+++ tests/get-server-1-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the `get-server` command.
+#    
+#/ usage:  rerun stubbs:test -m gogrid -p get-server [--answers <>]
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "get-server"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
